c05ba7348e8d6f16d855c423470d7d8f10fd0a5b
unknown <akshat.aksharma@gmail.com>
2021-01-11 15:26:25 +0530

    linked list with inserting at the start function

---
+++ LinkedLlist.py
@@ -0,0 +1,35 @@
+class Node:
+    #Class for creating a Node which contains the data and the next address 
+    #If this is the first node hence the next value will be none
+    def __init__(self,data=None,next=None):
+        self.data = data
+        self.next = next
+class LinkedList:
+    #Creating a class for LinkedList which contains various functions 
+    def __init__(self):
+        #head of the Linked List
+        self.head = None
+    
+    def insert_at_start(self,data):
+        #Function for inserting at the starting 
+        new_node = Node(data,self.head) #creating a new node 
+        self.head = new_node #now head will point to newly created node
+    def print(self): #funtion for prinitng the nodes and the datas
+        element = ''
+        if self.head is None:
+            element += 'Null'
+        temp = self.head
+        while temp: #looping through the head to check the next element 
+            element += str(temp.data)+ '--->'
+            temp = temp.next
+        print(element)
+
+            
+if __name__ == '__main__':
+    ll = LinkedList()  
+    ll.insert_at_start(2)
+    ll.insert_at_start(3)
+    ll.insert_at_start(4)
+    ll.insert_at_start(5)
+    ll.print()
+
