e3b818613d8ab5a33e31d01329b4620dc86cf0a1
unknown <akshat.aksharma@gmail.com>
2021-01-11 16:05:48 +0530

    Delete at any Index using the length of the Linked List

--- LinkedLlist.py
+++ LinkedLlist.py
@@ -23,6 +23,31 @@ class LinkedList:
         while temp.next:
             temp = temp.next
         temp.next = Node(data,None)
+    
+    def find_length(self):
+        temp = self.head
+        count = 0
+        while temp:
+            count += 1
+            temp = temp.next
+        #print(count)
+        return count
+    
+    def delete_at_index(self,index):
+        if index < 0 or index >= self.find_length():
+            raise Exception("Invalid Index")
+        if index == 0 :
+            self.head = self.head.next
+            return
+        count = 0 
+        temp = self.head
+        while temp:
+            if count == index - 1:
+                temp.next = temp.next.next
+                break
+            temp = temp.next
+            count += 1          
+
     def print(self): #funtion for prinitng the nodes and the datas
         element = ''
         if self.head is None:
@@ -32,7 +57,7 @@ class LinkedList:
             element += str(temp.data)+ '--->'
             temp = temp.next
         print(element)
-
+        
             
 if __name__ == '__main__':
     ll = LinkedList()  
@@ -41,5 +66,8 @@ if __name__ == '__main__':
     ll.insert_at_start(4)
     ll.insert_at_end(5)
     ll.insert_at_end(9)
+    ll.find_length()
+    #ll.delete_at_index(9)
+    ll.delete_at_index(2)
     ll.print()
 
